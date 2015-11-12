9238716149a12d6590d2973f4449f681ddd2aae5
znss1989 <znss1989@gmail.com>
2015-11-12 19:59:43 +0800

    Cat dual

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,13 @@
-var i = 0;
-$('#cat-img').click(function(e) {
-    ++i;
-    $('#poke-count').text('You\'ve poked this little cute kitten ' + i + ' times!!');
+var i1 = 0, i2 = 0;
+$('#cat-img1').click(function(e) {
+    var catName = 'Tom';
+    ++i1;
+    $('#cat-p1').text('You\'ve poked ' + catName+ ' ' + i1 + ' times!!');
 });
+$('#cat-img2').click(function(e) {
+    var catName = 'Tiger';
+    ++i2;
+    $('#cat-p2').text('You\'ve poked ' + catName + ' ' + i2 + ' times!!');
+});
+
+
